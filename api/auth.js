310711489ee60e4c369340a4544db433c8f2e99c
Dharun N <ndharun55@gmail.com>
2026-05-04 14:33:36 +0530

    Enable Vercel Admin Panel and OAuth

---
+++ api/auth.js
@@ -0,0 +1,24 @@
+const { AuthorizationCode } = require('simple-oauth2');
+
+const client = new AuthorizationCode({
+  client: {
+    id: process.env.OAUTH_CLIENT_ID,
+    secret: process.env.OAUTH_CLIENT_SECRET,
+  },
+  auth: {
+    tokenHost: 'https://github.com',
+    tokenPath: '/login/oauth/access_token',
+    authorizePath: '/login/oauth/authorize',
+  },
+});
+
+module.exports = async (req, res) => {
+  const authorizationUri = client.authorizeURL({
+    redirect_uri: `https://${req.headers.host}/callback`,
+    scope: 'repo,user',
+    state: Math.random().toString(36).substring(2),
+  });
+
+  res.writeHead(302, { Location: authorizationUri });
+  res.end();
+};
---
+++ api/callback.js
@@ -0,0 +1,50 @@
+const { AuthorizationCode } = require('simple-oauth2');
+
+const client = new AuthorizationCode({
+  client: {
+    id: process.env.OAUTH_CLIENT_ID,
+    secret: process.env.OAUTH_CLIENT_SECRET,
+  },
+  auth: {
+    tokenHost: 'https://github.com',
+    tokenPath: '/login/oauth/access_token',
+    authorizePath: '/login/oauth/authorize',
+  },
+});
+
+module.exports = async (req, res) => {
+  const { code } = req.query;
+
+  try {
+    const accessToken = await client.getToken({
+      code,
+      redirect_uri: `https://${req.headers.host}/callback`,
+    });
+
+    const token = accessToken.token.access_token;
+    
+    // This response is what Decap CMS expects
+    const content = `
+      <script>
+        const receiveMessage = (message) => {
+          window.opener.postMessage(
+            'authorization:github:success:${JSON.stringify({
+              token: token,
+              provider: 'github'
+            })}',
+            message.origin
+          );
+          window.removeEventListener('message', receiveMessage, false);
+        }
+        window.addEventListener('message', receiveMessage, false);
+        window.opener.postMessage('authorizing:github', '*');
+      </script>
+    `;
+
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send(content);
+  } catch (error) {
+    console.error('Access Token Error', error.message);
+    res.status(500).json(error.message);
+  }
+};
